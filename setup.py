889b7a1449d9179e47c3a04e3d7042e89984b184
Shawn Lovelidge <github@lernendercorp.com>
2023-08-16 11:42:42 -0500

    commit

--- setup.py
+++ setup.py
@@ -15,12 +15,14 @@ setup(
     author="Lovelidge, Shawn",
     author_email="slovelidge@lernendercorp.com",
     license="MIT",
+    keywords=['python', 'core'],
     classifiers=[
+        "Development Status :: 1 - Planning",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
     ],
-    install_requires=["bson >= 0.5.10"],
+    install_requires=["bson >= 0.5.10", "uuid >= 1.30", "uuid64 >= 0.2.0"],
     extras_require={
         "dev": ["pytest>=7.0", "twine>=4.0.2"],
     },
