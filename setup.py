af17eb48d9decff039afca5c4a956ef077054e72
Shawn Lovelidge <github@lernendercorp.com>
2023-08-16 13:29:16 -0500

    push

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("lib/Readme.md", "r") as f:
 
 setup(
     name="ln.py.core",
-    version="1.0.3",
+    version="1.0.2",
     description="Core Python Library",
     package_dir={"": "lib"},
     packages=find_packages(where="lib"),
